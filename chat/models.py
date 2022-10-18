6017556c011a373d709cc985e1515ce0ff538cbe
Bekzodbek <97502548+Bekzodbek2006@users.noreply.github.com>
2022-10-18 15:16:02 +0500

    bugs fixed

--- chat/models.py
+++ chat/models.py
@@ -5,3 +5,13 @@ class CustomeUser(AbstractUser):
     is_organiser = models.BooleanField(default=False)
     is_agent = models.BooleanField(default=False)
 
+
+
+class Chat(models.Model):
+    author = models.ForeignKey(CustomeUser, on_delete=models.CASCADE)
+    message = models.CharField(max_length=200)
+    img = models.ImageField(blank=True)
+    file = models.FileField(blank=True)
+
+    def __str__(self):
+        return f"id: {self.pk} message: {self.message}"
