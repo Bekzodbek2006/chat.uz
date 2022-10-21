3737d5d3ea706a272ebfe1c5049dc4130e84c28d
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-10-21 17:13:34 +0500

    added forms

--- chat/form.py
+++ chat/form.py
@@ -1,1 +1,23 @@
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.contrib.auth import get_user_model
 from django import forms
+from .models import *
+
+
+User = get_user_model()
+
+class Registration(UserCreationForm):
+     class Meta:
+        model = User
+        fields = ("username",)
+        field_classes = {"username": UsernameField}
+
+class CreateChatForm(forms.ModelForm):
+    class Meta:
+        model = Chat
+        fields = "__all__"
+
+class SendMsgForm(forms.ModelForm):
+    class Meta:
+        model = ChatMsg
+        fields = "__all__"
